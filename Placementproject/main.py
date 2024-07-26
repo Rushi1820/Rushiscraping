6a61d7dd11e7fddcf6f3376226cd8f9addb8243f
rushi <rushivardhan18@gmail.com>
2024-07-26 17:13:27 +0530

    added cors

--- Placementproject/main.py
+++ Placementproject/main.py
@@ -1,19 +1,29 @@
 import logging
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-logger.debug("Starting FastAPI app")
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from models import Base
 from database import engine
 import job_postings
 import uvicorn
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+logger.debug("Starting FastAPI app")
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+# Allow all origins for testing purposes. In production, specify the allowed origins.
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(job_postings.router, prefix="/api/v1", tags=["job_postings"])
 
