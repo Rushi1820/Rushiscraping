15af368d50309718524afc604f9fd36da13a7897
Rushivardhan <103994920+Rushi1820@users.noreply.github.com>
2025-03-31 09:28:23 +0530

    Update database.py

--- Placementproject/database.py
+++ Placementproject/database.py
@@ -3,8 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from databases import Database
 
-DATABASE_URL = "postgresql://scrapingdatabase_user:P7dMq2qFg3t0pVrDUVWgWL30liTi9IET@dpg-cqgt4p2ju9rs73ebui8g-a.singapore-postgres.render.com/scrapingdatabase"
-
+DATABASE_URL = "postgresql://rushi:GHdFDzD5lHwS4YUbOfMY7x3jmmOPzXEH@dpg-cvl14r3uibrs73a7r2dg-a.oregon-postgres.render.com/jobscraper"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
